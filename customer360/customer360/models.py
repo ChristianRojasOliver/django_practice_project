81aa01c6ab9013973c45fc8b4e7203cc5dc10a18
ChristianRojasOliver <christian.rojas201@gmail.com>
2025-01-28 12:59:50 -0500

    task2: create models

---
+++ customer360/customer360/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id)
+
